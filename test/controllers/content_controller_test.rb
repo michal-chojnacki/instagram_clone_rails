6dc1fc572e0e54f7fca8a9d63dd6a6f77755425d
Michal Chojnacki <michal.chojnacki@intive.com>
2020-10-05 16:56:26 +0200

    Implements tests

--- test/controllers/content_controller_test.rb
+++ test/controllers/content_controller_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ContentControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "when get main_content without api key then response should be unauthorized" do
+    # when
+    get '/api/v1/main_content'
+    
+    # then
+    assert_response :unauthorized
+  end
+
+  test "given user when get main_content with valid jwt then response should be authorized empty response" do
+    # given
+    user = User.find_by(username: "username_1")
+    jsonWebToken = JsonWebToken.encode(user_id: user.id)
+    expected = { contents:  Content.all, page:0, pages: 1}.to_json
+
+    # when
+    get '/api/v1/main_content', headers: { ApiKey: ENV['MOBILE_APP_KEY'], Authorization: "Bearer: #{jsonWebToken}" }
+    
+    # then
+    assert_response :success
+    assert_equal expected, @response.body
+  end
 end
